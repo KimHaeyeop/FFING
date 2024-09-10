56e95dc8f9fcffd0f01764983d03a7a54d54c1ad
Myeonghwa Kim <mhwazz@gmail.com>
2024-09-10 11:30:37 +0900

    feat: 프로젝트 생성 및 설정
    - 백엔드 프로젝트 생성
    - Querydsl 설정

---
+++ Backend/src/main/java/com/tbtr/ffing/FfingApplication.java
@@ -0,0 +1,13 @@
+package com.tbtr.ffing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FfingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FfingApplication.class, args);
+	}
+
+}
