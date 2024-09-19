f956258511eeaeb7588d98f4d037d4bb28265162
이규석 <didie8520@gmail.com>
2024-09-19 16:13:44 +0900

    feat: 펫 idle 상태 구현
    - 펫 이미지 불러오기
    - 배경 이미지 불러오기
    - 펫, 배경 이미지 public에 추가
    - phaser로 게임 컨테이너 생성
    - phaser로 splite sheet 쪼개서 구현

--- Frontend/FFING/dev-dist/sw.js
+++ Frontend/FFING/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.s2k0rhf2vn"
+    "revision": "0.53pb1if0su8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
