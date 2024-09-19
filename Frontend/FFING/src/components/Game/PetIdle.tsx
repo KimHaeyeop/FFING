36b6a65b9eddc07020affc7284de5a7efc0bc952
원민혁 <wmy4534@gmail.com>
2024-09-19 15:25:18 +0900

    feat: 컴포넌트 폴더 분리

---
+++ Frontend/FFING/src/components/Game/PetIdle.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PetIdle: React.FC = () => {
+  return (
+    <>
+      <div>
+        PetIdle
+      </div>
+    </>
+  );
+};
+
+export default PetIdle;
---
+++ Frontend/FFING/src/components/Game/PetStatusChart.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PetStatusChart: React.FC = () => {
+  return (
+    <>
+      <div>
+        PetStatusChart
+      </div>
+    </>
+  );
+};
+
+export default PetStatusChart;
