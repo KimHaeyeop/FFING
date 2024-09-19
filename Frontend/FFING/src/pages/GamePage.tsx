066f82524997b07cc202aa17a768f2b37760e4f7
원민혁 <wmy4534@gmail.com>
2024-09-19 15:25:18 +0900

    feat: 컴포넌트 폴더 분리

--- Frontend/FFING/src/pages/GamePage.tsx
+++ Frontend/FFING/src/pages/GamePage.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import NavBar from "../components/Common/Navbar";
+import PetIdle from "../components/Game/PetIdle";
+import PetStatusChart from "../components/Game/PetStatusChart";
 
 const GamePage: React.FC = () => {
   return (
     <>
       <div>
         게임페이지
+      </div>
+      <div>
+        <PetIdle />
+      </div>
+      <div>
+        <PetStatusChart />
+      </div>
+      <div>
+        <button> 게임 시작 | 랭킹 </button>
       </div>
       <NavBar />
     </>
