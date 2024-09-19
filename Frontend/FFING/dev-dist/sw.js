045f5af3b54768fb7243e002d798510560cf6972
원민혁 <wmy4534@gmail.com>
2024-09-19 15:05:07 +0900

    feat: 메인페이지, 게임페이지, 네비바 구성 npm install react-router-dom

--- Frontend/FFING/dev-dist/sw.js
+++ Frontend/FFING/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.rm6kfd3ali"
+    "revision": "0.47eue1ie64g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
