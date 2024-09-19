093922903307c284e75913064d6038095acf6cda
이규석 <didie8520@gmail.com>
2024-09-19 18:30:09 +0900

    Merge branch 'frontend/dev/3' into 'frontend/dev/5'

--- Frontend/FFING/dev-dist/sw.js
+++ Frontend/FFING/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.6p79uogq458"
+    "revision": "0.3m66v18vre8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
