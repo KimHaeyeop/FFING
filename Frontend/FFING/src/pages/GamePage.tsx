46dafebcf422dc2a7a002ee2b430d609b3e4f7f4
이규석 <didie8520@gmail.com>
2024-09-20 10:26:45 +0900

    feat: 게임 시작 및 랭킹 버튼 생성
    - ttf 파일 추가
    - index.css을 통해 전역으로 폰트 사용 가능하게 추가
    - 버튼 css 추가

--- Frontend/FFING/src/pages/GamePage.tsx
+++ Frontend/FFING/src/pages/GamePage.tsx
@@ -15,9 +15,26 @@ const GamePage: React.FC = () => {
       <div>
         <PetStatusChart />
       </div>
-      <div>
-        <button> 게임 시작 | 랭킹 </button>
+
+      {/* 버튼 영역 */}
+      <div className="flex rounded-lg overflow-hidden w-full max-w-screen-md mx-auto mt-6 h-16">
+        {/* 게임 시작 버튼 */}
+        <button
+          className="flex-grow-[7] bg-[#FFD874] text-black py-2 rounded-l-lg font-galmuri-11-bold"
+          // onClick={() => { /* 게임 시작 라우팅 */ }}
+        >
+          게임 시작
+        </button>
+
+        {/* 랭킹 버튼 */}
+        <button
+          className="flex-grow-[3] bg-[#FFA1A1] text-black py-2 rounded-r-lg font-galmuri-11-bold"
+          // onClick={() => { /* 랭킹 라우팅 */ }}
+        >
+          랭킹
+        </button>
       </div>
+
       <footer>
         <NavBar />
       </footer>
