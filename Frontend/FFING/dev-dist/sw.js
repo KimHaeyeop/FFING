316550292e18f6c837126a45f4bcec2d71b35818
이규석 <didie8520@gmail.com>
2024-09-22 20:12:58 +0900

    feat: 승자 표시 화면 추가
    - HP를 10으로 임의 조정
    - 공격마다 상대 체력을 확인하여 0이 되었으면 승자를 할당

--- Frontend/FFING/dev-dist/sw.js
+++ Frontend/FFING/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.6afr8kakblg"
+    "revision": "0.j9uqi7a77v8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
