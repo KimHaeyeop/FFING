80719ca6e5304861615852c9125a44e3a1895453
이규석 <didie8520@gmail.com>
2024-09-24 10:16:40 +0900

    chore: 기절 아이콘 추가 코드 작성

--- Frontend/FFING/dev-dist/sw.js
+++ Frontend/FFING/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.0ih4cn6jp9g"
+    "revision": "0.5j62j704qbg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
