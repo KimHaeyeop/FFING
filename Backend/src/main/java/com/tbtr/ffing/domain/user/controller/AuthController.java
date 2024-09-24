7ca2a442c20214a4ad48d88a33963fc31f62997a
김해엽 <rjssod2@naver.com>
2024-09-24 13:24:44 +0900

    #feat : Auth 관련, SecurityConfig 추가

---
+++ Backend/src/main/java/com/tbtr/ffing/domain/user/controller/AuthController.java
@@ -0,0 +1,11 @@
+package com.tbtr.ffing.domain.user.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ffing/auth")
+public class AuthController {
+
+
+}
---
+++ Backend/src/main/java/com/tbtr/ffing/domain/user/service/impl/AuthServiceImpl.java
@@ -0,0 +1,7 @@
+package com.tbtr.ffing.domain.user.service.impl;
+
+import com.tbtr.ffing.domain.user.service.AuthService;
+
+public class AuthServiceImpl implements AuthService {
+
+}
