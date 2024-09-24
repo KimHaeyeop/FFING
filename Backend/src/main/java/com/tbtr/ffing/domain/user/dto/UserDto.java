e109b203669e25f457e1e39a4f03766b5ecfce2e
탁인혁 <dlsgur1343@naver.com>
2024-09-24 12:51:12 +0900

    chore: 디렉토리 재구성

--- Backend/src/main/java/com/tbtr/ffing/domain/user/dto/UserDto.java
+++ Backend/src/main/java/com/tbtr/ffing/domain/user/dto/UserDto.java
@@ -1,16 +1,16 @@
-package com.tbtr.ffing.domain.user.dto;
-
-import lombok.Data;
-
-@Data
-public class UserDto {
-    public final long userSq;
-    public final String userId;
-    public final String userNm;
-
-    //생성자 생략
-
-    public static UserDto from(UserEntity user) {
-        return new UserDto(user.getUserSq(), user.getUserId(), user.getUserNm());
-    }
-}
+//package com.tbtr.ffing.domain.user.dto;
+//
+//import lombok.Data;
+//
+//@Data
+//public class UserDto {
+//    public final long userSq;
+//    public final String userId;
+//    public final String userNm;
+//
+//    //생성자 생략
+//
+//    public static UserDto from(UserEntity user) {
+//        return new UserDto(user.getUserSq(), user.getUserId(), user.getUserNm());
+//    }
+//}
