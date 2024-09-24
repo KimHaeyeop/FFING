bc3d2ec3b2173bd4eb3cb574dfc1694623dc83ab
김해엽 <rjssod2@naver.com>
2024-09-24 14:30:01 +0900

    #feat : JWT 관련 초기화 - 기존 파일 제거 - JWT 관련 기본 파일 추가

---
+++ Backend/src/main/java/com/tbtr/ffing/global/auth/JWTUtil.java
@@ -0,0 +1,20 @@
+package com.tbtr.ffing.global.auth;
+
+import io.jsonwebtoken.Jwts;
+import java.nio.charset.StandardCharsets;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JWTUtil {
+
+    private SecretKey secretKey;
+
+    public JWTUtil(@Value("${jwt.secret}") String secret) {
+        this.secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8),
+                Jwts.SIG.HS256.key().build().getAlgorithm());
+    }
+
+}
