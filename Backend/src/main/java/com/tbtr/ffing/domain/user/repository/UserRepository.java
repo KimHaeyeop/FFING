80c09cc3f1894b06a375d4a4ae8bf75e3ab1225e
김해엽 <rjssod2@naver.com>
2024-09-25 17:36:17 +0900

    feat: 회원가입 가입 및 테스트 일부 구현

--- Backend/src/main/java/com/tbtr/ffing/domain/user/repository/UserRepository.java
+++ Backend/src/main/java/com/tbtr/ffing/domain/user/repository/UserRepository.java
@@ -1,9 +1,9 @@
 package com.tbtr.ffing.domain.user.repository;
 
-
 import com.tbtr.ffing.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    boolean existsByNickname(String nickname);
 }
--- Backend/src/main/java/com/tbtr/ffing/domain/user/service/AuthService.java
+++ Backend/src/main/java/com/tbtr/ffing/domain/user/service/AuthService.java
@@ -1,5 +1,9 @@
 package com.tbtr.ffing.domain.user.service;
 
+import com.tbtr.ffing.domain.user.dto.UserInfoDto.Request;
+
 public interface AuthService {
+
+    void signup(Request requestDTO);
 
 }
