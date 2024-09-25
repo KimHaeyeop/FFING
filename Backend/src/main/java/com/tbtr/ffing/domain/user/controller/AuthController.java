387ae3eb4907bfe12ab5b1d760415c0ce8b8a9ca
김해엽 <rjssod2@naver.com>
2024-09-25 17:36:17 +0900

    feat: 회원가입 가입 및 테스트 일부 구현

--- Backend/src/main/java/com/tbtr/ffing/domain/user/controller/AuthController.java
+++ Backend/src/main/java/com/tbtr/ffing/domain/user/controller/AuthController.java
@@ -1,11 +1,33 @@
 package com.tbtr.ffing.domain.user.controller;
 
+import com.tbtr.ffing.domain.user.dto.UserInfoDto;
+import com.tbtr.ffing.domain.user.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/ffing/auth")
+@RequiredArgsConstructor
+@RequestMapping("/api/v1")
 public class AuthController {
 
+    private final AuthService authService;
 
+    @PostMapping("/signup")
+    public ResponseEntity<?> signup(@Valid @RequestBody UserInfoDto.Request requestDTO) {
+        System.out.println("nickname: " + requestDTO.getNickname());
+        authService.signup(requestDTO);
+        return ResponseEntity.ok("회원가입 성공");
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<?> test() {
+        System.out.println("test controller 진입");
+        return ResponseEntity.ok("test ok");
+    }
 }
--- Backend/src/main/java/com/tbtr/ffing/domain/user/service/impl/AuthServiceImpl.java
+++ Backend/src/main/java/com/tbtr/ffing/domain/user/service/impl/AuthServiceImpl.java
@@ -1,7 +1,35 @@
 package com.tbtr.ffing.domain.user.service.impl;
 
+import com.tbtr.ffing.domain.user.dto.UserInfoDto;
+import com.tbtr.ffing.domain.user.dto.UserInfoDto.Request;
+import com.tbtr.ffing.domain.user.entity.User;
+import com.tbtr.ffing.domain.user.repository.UserRepository;
 import com.tbtr.ffing.domain.user.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class AuthServiceImpl implements AuthService {
 
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final UserRepository userRepository;
+
+    @Override
+    public void signup(Request requestDTO) {
+        String nickname = requestDTO.getNickname();
+
+        System.out.println("nickname: " + nickname);
+        if (userRepository.existsByNickname(nickname)) {
+            return;
+        }
+
+        User userInfoDto = UserInfoDto.Request.toEntity(requestDTO, bCryptPasswordEncoder);
+
+        userRepository.save(userInfoDto);
+        System.out.println("saved user: " + userInfoDto);
+    }
 }
