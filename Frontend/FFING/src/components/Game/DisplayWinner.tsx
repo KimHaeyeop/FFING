7339a526ecb62b36d8be63b26b1f94fa77cf9323
이규석 <didie8520@gmail.com>
2024-09-26 10:39:45 +0900

    design: 승리 화면 색상, 내용 변경
    - 닉네임 삭제
    - 승리 시 #FAF7E1 배경색 설정
    - 패배 시 #EEFFF0 배경색 설정
    - phasergame에서 setwinner의 인자 수정

--- Frontend/FFING/src/components/Game/DisplayWinner.tsx
+++ Frontend/FFING/src/components/Game/DisplayWinner.tsx
@@ -5,12 +5,16 @@ interface DisplayWinnerProps {
 }
 
 const DisplayWinner: React.FC<DisplayWinnerProps> = ({ winner }) => {
+  const backgroundColor = winner === 'me' ? '#FAF7E1' : '#EEFFF0';
   return (
-    <div className="w-full max-w-lg mx-auto p-8 border-4 border-black rounded-lg bg-[#EEFFF0] flex flex-col justify-center items-center">
-      {/* 승자 닉네임 */}
-      <span className="text-3xl font-galmuri-11-bold mb-4">{winner}</span>
+    <div className="w-full max-w-lg mx-auto p-8 border-4 border-black rounded-lg bg-[#EEFFF0] flex flex-col justify-center items-center" style={{backgroundColor}}>
       {/* 승리 텍스트 */}
-      <span className="text-3xl font-galmuri-11-bold">Win!</span>
+      <span className="text-3xl font-galmuri-11-bold mb-4">You</span>
+      {winner === 'me' ? (
+        <span className="text-3xl font-galmuri-11-bold">Win!</span>
+      ) : (
+        <span className="text-3xl font-galmuri-11-bold">Lose!</span>
+      )}
     </div>
   );
 };
