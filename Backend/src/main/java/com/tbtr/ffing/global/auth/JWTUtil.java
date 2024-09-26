0e63d8f0d35af26ed8aca5ee3dddb81b99ad33f6
김해엽 <rjssod2@naver.com>
2024-09-26 17:57:00 +0900

    feat: JWTFIlter, Util 구현 - doFilterInternal 구현 - 토큰 만료시 로직 추가, 예외 처리 구현 필요

--- Backend/src/main/java/com/tbtr/ffing/global/auth/JWTUtil.java
+++ Backend/src/main/java/com/tbtr/ffing/global/auth/JWTUtil.java
@@ -1,7 +1,10 @@
 package com.tbtr.ffing.global.auth;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import java.nio.charset.StandardCharsets;
+import java.util.Date;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,11 +13,73 @@ import org.springframework.stereotype.Component;
 @Component
 public class JWTUtil {
 
-    private SecretKey secretKey;
+    private final SecretKey secretKey;
+    @Value("${jwt.access.expiration}")
+    private Long ACCESS_TOKEN_EXPIRATION_PERIOD;
+    @Value("${jwt.refresh.expiration}")
+    private Long REFRESH_TOKEN_EXPIRATION_PERIOD;
 
     public JWTUtil(@Value("${jwt.secret}") String secret) {
         this.secretKey = new SecretKeySpec(secret.getBytes(StandardCharsets.UTF_8),
                 Jwts.SIG.HS256.key().build().getAlgorithm());
+    }
+
+    public String getUserId(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload()
+                   .get("userId", String.class);
+    }
+
+    public String getRole(String token) {
+
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload()
+                   .get("role", String.class);
+    }
+
+    public String getCategory(String token) {
+
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload()
+                   .get("category", String.class);
+    }
+
+    public Boolean isExpired(String token) {
+        System.out.println("JWTUtil: Checking if the token is expired..."); // 로그 추가
+        System.out.println("현재시간: " + new Date(System.currentTimeMillis()));
+
+        Date expirationDate = null;
+        try {
+            expirationDate = Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload()
+                                 .getExpiration();
+        } catch (ExpiredJwtException e) {
+            System.out.println("Token is expired: " + e.getMessage());
+            return true; // 토큰이 만료된 경우 true 반환
+        } catch (JwtException e) {
+            System.out.println("JWT parsing error: " + e.getMessage());
+            return true; // 파싱 에러가 발생한 경우도 true 반환
+        }
+
+        System.out.println("expirationDate: " + expirationDate);
+        Date currentDate = new Date();
+
+        // 토큰의 만료 날짜와 현재 날짜를 출력
+        System.out.println("Token Expiration Date: " + expirationDate);
+        System.out.println("Current Date: " + currentDate);
+
+        return expirationDate.before(currentDate);
+    }
+
+    public String createJwt(String category, Long userId, String role) {
+
+        Long expirationPeriod =
+                category.equals("access") ? ACCESS_TOKEN_EXPIRATION_PERIOD : REFRESH_TOKEN_EXPIRATION_PERIOD;
+
+        return Jwts.builder()
+                   .claim("category", category)
+                   .claim("userId", userId.toString())
+                   .claim("role", role)
+                   .issuedAt(new Date(System.currentTimeMillis()))
+                   .expiration(new Date(System.currentTimeMillis() + expirationPeriod * 60 * 1000L))
+                   .signWith(secretKey)
+                   .compact();
     }
 
 }
--- Backend/src/main/java/com/tbtr/ffing/global/config/SecurityConfig.java
+++ Backend/src/main/java/com/tbtr/ffing/global/config/SecurityConfig.java
@@ -1,22 +1,34 @@
 package com.tbtr.ffing.global.config;
 
-import jakarta.servlet.http.HttpServletResponse;
+import com.tbtr.ffing.domain.user.repository.UserRepository;
+import com.tbtr.ffing.global.auth.JWTFilter;
+import com.tbtr.ffing.global.auth.JWTUtil;
+import com.tbtr.ffing.global.redis.repository.RedisRefreshTokenRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 @Log4j2
 public class SecurityConfig {
+
+    //AuthenticationManager가 인자로 받을 AuthenticationConfiguraion 객체 생성자 주입
+    private final AuthenticationConfiguration authenticationConfiguration;
+    private final JWTUtil jwtUtil;
+    private final UserRepository userRepository;
+    private final RedisRefreshTokenRepository redisRefreshTokenRepository;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -25,23 +37,26 @@ public class SecurityConfig {
             .formLogin(AbstractHttpConfigurer::disable)
             .httpBasic(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests((auth) -> auth
-                    .anyRequest().permitAll()) // 임시로 모두 허용
-//                    .requestMatchers("/", "/signup", "/test").permitAll()
+                    .requestMatchers("/", "/auth/signup", "/auth/signin").permitAll()
+                    .anyRequest().authenticated())
+//                    .anyRequest().permitAll()) // 임시로 모두 허용
 //                    .requestMatchers("/admin/**").hasRole("ADMIN")
-//                    .anyRequest().authenticated())
             .sessionManagement(session -> session
-                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 관리 설정
-            .exceptionHandling(handling -> handling
-                    .accessDeniedHandler((request, response, accessDeniedException) -> {
-                        log.error("Access Denied for request: " + request.getRequestURI());
-                        response.sendError(HttpServletResponse.SC_FORBIDDEN, "Access Denied");
-                    })); // 접근 거부 처리
-
+                    .sessionCreationPolicy(SessionCreationPolicy.STATELESS)); // 세션 관리 설정
+//        // jwt 토큰 확인 필터
+        http.addFilterBefore(new JWTFilter(jwtUtil, userRepository, redisRefreshTokenRepository),
+                UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+
+        return configuration.getAuthenticationManager();
     }
 }
