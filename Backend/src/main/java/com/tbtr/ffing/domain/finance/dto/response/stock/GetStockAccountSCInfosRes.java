7d808f2f5c884959a217c1c195ad8b3ebf043c75
변재호 <bjho606@naver.com>
2024-09-26 16:05:42 +0900

    feat: 증권사들의 거래 내역 개요 불러오기
    - 한 사용자의 특정 증권사의 각 종목들에 대한 개요 불러오기
    	- 해당 종목의 현재가
    	- 해당 종목을 갖고 있는 수량
    	- 해당 종목을 현재까지 매수했던 총 금액 (투자 수익률을 위한 것뿐 쓸일 없음)
    	- 현재까지 총 수익 금액
    	- 현재까지 수익률 (총 수익 금액 / 지금까지 매수했던 총 금액) *
    	  100

---
+++ Backend/src/main/java/com/tbtr/ffing/domain/finance/dto/response/stock/GetStockAccountSCInfosRes.java
@@ -0,0 +1,22 @@
+package com.tbtr.ffing.domain.finance.dto.response.stock;
+
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@ToString
+public class GetStockAccountSCInfosRes {
+
+    private List<GetStockAccountSCInfoRes> stockAccountInfos;
+
+    public static GetStockAccountSCInfosRes of(Map<Long, GetStockAccountSCInfoRes> stockAccountMap) {
+        List<GetStockAccountSCInfoRes> toStockAccountInfoList = new ArrayList<>(stockAccountMap.values());
+        return new GetStockAccountSCInfosRes(toStockAccountInfoList);
+    }
+}
--- Backend/src/main/java/com/tbtr/ffing/domain/finance/service/impl/StockServiceImpl.java
+++ Backend/src/main/java/com/tbtr/ffing/domain/finance/service/impl/StockServiceImpl.java
@@ -1,8 +1,11 @@
 package com.tbtr.ffing.domain.finance.service.impl;
 
+import com.tbtr.ffing.domain.finance.dto.response.stock.GetStockAccountSCInfoRes;
 import com.tbtr.ffing.domain.finance.dto.response.stock.GetStockAccountInfoRes;
 import com.tbtr.ffing.domain.finance.dto.response.stock.GetStockAccountInfosRes;
+import com.tbtr.ffing.domain.finance.dto.response.stock.GetStockAccountSCInfosRes;
 import com.tbtr.ffing.domain.finance.entity.StockAccount;
+import com.tbtr.ffing.domain.finance.entity.StockTransaction;
 import com.tbtr.ffing.domain.finance.repository.StockInfoRepository;
 import com.tbtr.ffing.domain.finance.repository.StockAccountRepository;
 import com.tbtr.ffing.domain.finance.repository.StockTransactionRepository;
@@ -12,8 +15,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.ArrayList;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -33,5 +40,72 @@ public class StockServiceImpl implements StockService {
                 .toList();
 
         return GetStockAccountInfosRes.of(stockAccountInfoResList);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public GetStockAccountSCInfosRes getStockAccountSCInfos(Long ssafyUserId, Long stockAccountId) {
+        List<StockTransaction> transactions = stockTransactionRepository.findByStockAccountIdWithFetchJoin(stockAccountId);
+        System.out.println(transactions);
+
+        Map<Long, GetStockAccountSCInfoRes> stockAccountMap = new HashMap<>();
+        for (StockTransaction transaction : transactions) {
+//            String key = transaction.getStockInfo().getStockCode() + "|" + transaction.getStockInfo().getStockName();
+            Long key = transaction.getStockInfo().getStockInfoId();
+
+            BigDecimal purchasePrice = transaction.getTransactionBalance();
+            BigDecimal sumPurchasePrice;
+            Long purchaseQuantity = transaction.getTransactionQuantity();
+
+            BigDecimal curEvaluationAmount = transaction.getStockInfo().getCurrentEvaluationPrice().multiply(BigDecimal.valueOf(purchaseQuantity));
+            BigDecimal purchaseAmount = purchasePrice.multiply(BigDecimal.valueOf(purchaseQuantity));
+            if ("2".equals(transaction.getTransactionType())) {
+                curEvaluationAmount = curEvaluationAmount.negate();
+                purchaseAmount = purchaseAmount.negate();
+                purchaseQuantity = purchaseQuantity * -1;
+                purchasePrice = BigDecimal.valueOf(0);
+            }
+            BigDecimal plBalance = purchaseAmount.subtract(curEvaluationAmount);
+            sumPurchasePrice = purchasePrice.multiply(BigDecimal.valueOf(purchaseQuantity));
+//            System.out.println(key + ": " + purchaseQuantity + ", " + purchaseAmount + ", " + curEvaluationAmount + ", " + plBalance);
+
+            GetStockAccountSCInfoRes scInfo = stockAccountMap.get(key);
+            // 종목 정보가 최초라면, map에 추가
+            if (scInfo == null) {
+                scInfo = GetStockAccountSCInfoRes.builder()
+                        .stockAccountId(transaction.getStockAccount().getStockAccountId())
+                        .securitiesCompanyCode(transaction.getStockInfo().getStockCode())
+                        .securitiesCompanyName(transaction.getStockInfo().getStockName())
+                        .totalSumEvaluationAmount(curEvaluationAmount)
+                        .totalStockQuantity(purchaseQuantity)
+                        .totalSumPurchaseAmount(sumPurchasePrice)
+                        .totalPLBalance(plBalance)
+                        .totalPLRate(getPLRate(plBalance, sumPurchasePrice))
+                        .build();
+            }
+            // 종목 정보가 최초가 아니라면, map에서 갱신
+            else {
+                curEvaluationAmount = scInfo.getTotalSumEvaluationAmount().add(curEvaluationAmount);
+                sumPurchasePrice = scInfo.getTotalSumPurchaseAmount().add(sumPurchasePrice);
+                plBalance = purchaseAmount.add(plBalance);
+                purchaseQuantity = scInfo.getTotalStockQuantity() + purchaseQuantity;
+                scInfo.setTotalSumEvaluationAmount(curEvaluationAmount);
+                scInfo.setTotalStockQuantity(purchaseQuantity);
+                scInfo.setTotalSumPurchaseAmount(sumPurchasePrice);
+                scInfo.setTotalPLBalance(plBalance);
+                scInfo.setTotalPLRate(getPLRate(plBalance, sumPurchasePrice));
+//                System.out.println(key + ": " + purchaseQuantity + ", " + purchaseAmount + ", " + curEvaluationAmount + ", " + plBalance);
+            }
+
+            stockAccountMap.put(key, scInfo);
+        }
+//        System.out.println(stockAccountMap);
+        return GetStockAccountSCInfosRes.of(stockAccountMap);
+    }
+
+    // 수익률 = (수익금 / 평가금) * 100
+    private BigDecimal getPLRate(BigDecimal totalPLBalance, BigDecimal totalPurchaseAmount) {
+
+        return totalPLBalance.multiply(BigDecimal.valueOf(100)).divide(totalPurchaseAmount, 2, RoundingMode.HALF_EVEN);
     }
 }
