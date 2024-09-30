3aff1df56ad635b9666b8b7955294883e0be4165
이규석 <didie8520@gmail.com>
2024-09-30 11:09:49 +0900

    feat: 펫 기록에 레이더차트 생성

--- Frontend/FFING/src/components/Game/PetDetailModal.tsx
+++ Frontend/FFING/src/components/Game/PetDetailModal.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import PetStatusChart from './PetStatusChart'; // 레이더 차트 컴포넌트
-import PetRaderChart from './PetRaderChart'; // 테스트용
+import { Radar } from "react-chartjs-2";
+import 'chart.js/auto';
 import Modal from '../Common/Modal';
 import PetSprite from './PetSprite';
 
@@ -21,6 +21,61 @@ const PetDetailModal: React.FC<PetDetailModalProps> = ({
   petImageUrl,
   petTrait,
 }) => {
+  const currentWeek = { 식비: 6, 쇼핑: 8, 교통: 6, 생활: 6, 문화: 6 };
+  const previousWeek = { 식비: 8, 쇼핑: 6, 교통: 6, 생활: 8, 문화: 8 };
+
+  const labels = ['식비', '쇼핑', '교통', '생활', '문화'];
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: '이번 주',
+        data: Object.values(currentWeek),
+        backgroundColor: 'rgba(54, 162, 235, 0.2)', // 파란색
+        borderColor: 'rgba(54, 162, 235, 1)',
+        borderWidth: 1,
+      },
+      {
+        label: '저번 주',
+        data: Object.values(previousWeek),
+        backgroundColor: 'rgba(255, 99, 132, 0.2)', // 빨간색
+        borderColor: 'rgba(255, 99, 132, 1)',
+        borderWidth: 1,
+      },
+    ],
+  };
+
+  const options = {
+    plugins: {
+      legend: {
+        display: false,  // 차트 위에 라벨을 숨김
+      },
+    },
+    scales: {
+      r: {
+        angleLines: {
+          display: false,
+        },
+        suggestedMin: 0,
+        suggestedMax: 10,
+        ticks: {
+          stepSize: 2,          // 각 능력치 간격 조정
+          backdropPadding: 0,    // 레이블과 배경 패딩 없앰
+          max: 10,               // 최대값 설정
+          min: 0,                // 최소값 설정
+        },
+        pointLabels: {
+          padding: 0,           // 능력치 레이블과 중앙 사이 간격 줄임
+          font: {
+            size: 10,
+            weight: 'bold'
+          }
+        },
+      },
+    },
+  };
+  
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <div className="flex flex-col items-center">
@@ -42,7 +97,15 @@ const PetDetailModal: React.FC<PetDetailModalProps> = ({
 
         {/* 레이더 차트 (펫 능력치) */}
         <div className="mt-6 w-full">
-          <PetRaderChart />
+          <div className="flex justify-center items-center">
+            {/* 카드 스타일 적용 */}
+            <div className="shadow-lg rounded-lg p-0 max-w-4xl w-full flex flex-row">
+              {/* 레이더 차트 */}
+              <div className="w-2/3 flex justify-center items-center p-1">
+                <Radar data={data} options={options} />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </Modal>
