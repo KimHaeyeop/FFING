55c576b178d1ca95abe18ce87ac6b005ba34670d
이규석 <didie8520@gmail.com>
2024-10-01 13:16:29 +0900

    feat: MainPage 화면 구성

--- Frontend/FFING/src/pages/MainPage.tsx
+++ Frontend/FFING/src/pages/MainPage.tsx
@@ -1,19 +1,34 @@
 import React from "react";
-import Header from '../components/Common/LinkHeader'
+import TextHeader from '../components/Common/TextHeader'
 import NavBar from "../components/Common/Navbar";
+import useViewportStore from "../store/useViewportStore";
 
 const MainPage: React.FC = () => {
+  const dvw = useViewportStore((state) => state.dvw);
+  const dvh = useViewportStore((state) => state.dvh);
+
   return (
     <div className="flex justify-center items-center">
       <div className="w-screen h-screen">
-        <header>
-          {/* route 실험 */}
-          <Header contentName="지출" contentRoute="/game"/> 
+        <header style={{height: `${dvh * 5}px`}}>
+          {/* 사용자의 정보와 알람 */}
+          <TextHeader title="이규석 님"/> 
         </header>
-        <div>
-          메인페이지
-        </div>
-        <footer>
+        <main style={{height: `${dvh * 80}px`}}>
+          {/* 자산 목표 달성 관련 */}
+          <div style={{height: '20%'}}>
+            목표 달성까지
+          </div>
+          {/* 게임 화면 관련 */}
+          <div style={{height: '40%'}}>
+            펫 화면
+          </div>
+          {/* 지출 내역 관련 */}
+          <div style={{height: '40%'}}>
+            이번달 지출 내역
+          </div>
+        </main>
+        <footer style={{height: `${dvh * 15}px`}}>
           <NavBar />
         </footer>
       </div>
