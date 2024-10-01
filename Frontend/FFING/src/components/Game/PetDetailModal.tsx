b095a5bbbe843d96bcd660a5144489c704f3db73
이규석 <didie8520@gmail.com>
2024-10-01 09:07:24 +0900

    design: 도감 페이지 디자인 변경
    - figma에 맞게 요소 배치, 크기 조정
    - dvw, dvh를 활용한 화면 크기 조정

--- Frontend/FFING/src/components/Game/PetDetailModal.tsx
+++ Frontend/FFING/src/components/Game/PetDetailModal.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import { Radar } from "react-chartjs-2";
 import 'chart.js/auto';
-import Modal from '../Common/Modal';
 import PetSprite from './PetSprite';
+import useViewportStore from '../../store/useViewportStore';
 
 interface PetDetailModalProps {
   isOpen: boolean;
   onClose: () => void;
   petName: string;
-  petIndex: number; // 이것도 자체적으로 펫 인덱스가 있을 수도 있다.
+  petIndex: number;
   petImageUrl: string;
-  petTrait: string; // 특성이 여러 개일수도 있어서 배열로 바뀔 수도 있다. 아니면 펫 상세 정보에서 trait도 가지고 있을 수도 있다.
+  petTrait: string;
 }
 
 const PetDetailModal: React.FC<PetDetailModalProps> = ({
@@ -21,8 +21,11 @@ const PetDetailModal: React.FC<PetDetailModalProps> = ({
   petImageUrl,
   petTrait,
 }) => {
+
+  const dvw = useViewportStore((state) => state.dvw); 
+  const dvh = useViewportStore((state) => state.dvh);
+
   const currentWeek = { 식비: 6, 쇼핑: 8, 교통: 6, 생활: 6, 문화: 6 };
-  const previousWeek = { 식비: 8, 쇼핑: 6, 교통: 6, 생활: 8, 문화: 8 };
 
   const labels = ['식비', '쇼핑', '교통', '생활', '문화'];
 
@@ -32,15 +35,8 @@ const PetDetailModal: React.FC<PetDetailModalProps> = ({
       {
         label: '이번 주',
         data: Object.values(currentWeek),
-        backgroundColor: 'rgba(54, 162, 235, 0.2)', // 파란색
+        backgroundColor: 'rgba(54, 162, 235, 0.2)', 
         borderColor: 'rgba(54, 162, 235, 1)',
-        borderWidth: 1,
-      },
-      {
-        label: '저번 주',
-        data: Object.values(previousWeek),
-        backgroundColor: 'rgba(255, 99, 132, 0.2)', // 빨간색
-        borderColor: 'rgba(255, 99, 132, 1)',
         borderWidth: 1,
       },
     ],
@@ -49,7 +45,7 @@ const PetDetailModal: React.FC<PetDetailModalProps> = ({
   const options = {
     plugins: {
       legend: {
-        display: false,  // 차트 위에 라벨을 숨김
+        display: false,
       },
     },
     scales: {
@@ -60,13 +56,12 @@ const PetDetailModal: React.FC<PetDetailModalProps> = ({
         suggestedMin: 0,
         suggestedMax: 10,
         ticks: {
-          stepSize: 2,          // 각 능력치 간격 조정
-          backdropPadding: 0,    // 레이블과 배경 패딩 없앰
-          max: 10,               // 최대값 설정
-          min: 0,                // 최소값 설정
+          stepSize: 2,
+          max: 10,
+          min: 0,
         },
         pointLabels: {
-          padding: 0,           // 능력치 레이블과 중앙 사이 간격 줄임
+          padding: 0,
           font: {
             size: 10,
             weight: 'bold'
@@ -75,40 +70,64 @@ const PetDetailModal: React.FC<PetDetailModalProps> = ({
       },
     },
   };
-  
+
+  // 모달이 열려있지 않으면 렌더링하지 않음
+  if (!isOpen) return null;
+
   return (
-    <Modal isOpen={isOpen} onClose={onClose}>
-      <div className="flex flex-col items-center">
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-10">
+      {/* 모달 창 */}
+      <div className="bg-white rounded-lg border-4 border-gray-400 relative"
+        style={{
+          width: `${70 * dvw}px`,   // dvw를 활용해 모달 너비 설정
+          height: `${70 * dvh}px`,  // dvh를 활용해 모달 높이 설정
+        }}
+      >
+        {/* 모달 닫기 버튼 */}
+        <button
+          onClick={onClose}
+          className="absolute top-2 right-2 text-gray-700"
+        >
+          X
+        </button>
+
         {/* 상단의 둥근 원형 영역 */}
-        <div className="w-24 h-24 bg-[#BBBBBB] rounded-full -mt-12 flex justify-center items-center relative z-10">
-          <PetSprite imageUrl={petImageUrl} isUnlocked={true}/>
+        <div 
+          className="bg-[#BBBBBB] w-48 h-48 rounded-full mt-12 flex justify-center items-center mx-auto"
+        >
+          <PetSprite imageUrl={petImageUrl} isUnlocked={true} />
         </div>
 
         {/* 펫 이름과 도감 인덱스 */}
-        <div className="mt-6 flex items-center">
-          <h2 className="text-lg font-bold mr-4">{petName}</h2>
-          <span className="text-gray-500">No.{petIndex}</span>
+        <div className="mt-[5%] flex justify-between px-1 items-center">
+          <h2 className="text-xl font-bold mr-4">{petName}</h2>
+          <span className="text-sm text-gray-500">No.{petIndex}</span>
         </div>
 
-        {/* 펫 특성 (Chip 형태 속성에 따라서 색상을 바꾸긴 해야 함?) */}
-        <div className="mt-2 bg-blue-200 text-blue-800 px-3 py-1 rounded-full">
+        {/* 펫 특성 (Chip 형태) */}
+        <div className="mt-3 bg-blue-200 text-blue-800 px-3 py-1 rounded-full inline-flex">
           {petTrait}
         </div>
 
         {/* 레이더 차트 (펫 능력치) */}
-        <div className="mt-6 w-full">
-          <div className="flex justify-center items-center">
-            {/* 카드 스타일 적용 */}
-            <div className="shadow-lg rounded-lg p-0 max-w-4xl w-full flex flex-row">
-              {/* 레이더 차트 */}
-              <div className="w-2/3 flex justify-center items-center p-1">
-                <Radar data={data} options={options} />
-              </div>
-            </div>
+        <div className="mt-6 flex justify-center items-center">
+          {/* 레이더 차트 */}
+          <div
+            style={{
+              height: `${70 * 0.4 * dvh}px`,  // dvh를 기반으로 차트 높이 설정
+              width: `${70 * dvw}px`,   // dvw를 기반으로 차트 너비 설정
+              // maxHeight: '100%',        // 모달을 벗어나지 않게 최대 높이 설정
+              // maxWidth: '100%'          // 모달을 벗어나지 않게 최대 너비 설정
+            }}>
+            <Radar 
+              data={data}
+              options={options}
+              className='mx-auto'
+            />
           </div>
         </div>
       </div>
-    </Modal>
+    </div>
   );
 };
 
--- Frontend/FFING/src/components/Game/RecordSection.tsx
+++ Frontend/FFING/src/components/Game/RecordSection.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import PetDetailModal from "./PetDetailModal";
 import PetRecord from "./PetRecord";
+import useViewportStore from "../../store/useViewportStore";
 
 interface petRecords {
   week: string;
@@ -13,6 +14,8 @@ interface petRecords {
 }
 
 const RecordSection: React.FC = () => {
+  const dvw = useViewportStore((state) => state.dvw);
+  const dvh = useViewportStore((state) => state.dvh); 
   const [isModalOpen, setModalOpen] = useState(false)
   // 모달에 띄울 데이터를 관리
   const [selectedPet, setSelectedPet] = useState({
@@ -24,10 +27,11 @@ const RecordSection: React.FC = () => {
 
   // 주차별 데이터 (API 완성되면 가져오기)
   const petRecords = [
-    { week: '9월 1주차', petName: 'bear-brown', wins: 10, losses: 3, petImageUrl: '/pets/bear-brown.png', petIndex: 1, petTrait: 'food' },
-    { week: '9월 2주차', petName: 'bear-lime', wins: 8, losses: 5, petImageUrl: '/pets/bear-lime.png', petIndex: 2, petTrait: 'living' },
-    { week: '9월 3주차', petName: 'candy-fluff-white', wins: 6, losses: 7, petImageUrl: '/pets/candy-fluff-white.png', petIndex: 3, petTrait: 'travel' },
-    // 더 많은 주차 데이터 추가 가능
+    { week: '9월 1주차', petName: 'bear-brown', wins: 10, losses: 3, petImageUrl: '/pets/bear-brown.png', petIndex: 1, petTrait: '외식' },
+    { week: '9월 2주차', petName: 'bear-lime', wins: 8, losses: 5, petImageUrl: '/pets/bear-lime.png', petIndex: 2, petTrait: '생활/문화' },
+    { week: '9월 3주차', petName: 'candy-fluff-white', wins: 6, losses: 7, petImageUrl: '/pets/candy-fluff-white.png', petIndex: 3, petTrait: '여행' },
+    { week: '9월 4주차', petName: 'candy-fluff-yellow', wins: 6, losses: 7, petImageUrl: '/pets/candy-fluff-yellow.png', petIndex: 4, petTrait: '교통' },
+    { week: '9월 5주차', petName: 'cat-black', wins: 6, losses: 7, petImageUrl: '/pets/cat-black.png', petIndex: 5, petTrait: '금융' },
   ];
 
   // 모달을 여는 함수
@@ -42,22 +46,27 @@ const RecordSection: React.FC = () => {
   })
   
   return (
-    <section className="mt-8">
+    <section className="mt-2 pb-20">
       {/* 현재 연도와 달 표시 (ex: 2024년 9월) API 활용 불가피? */}
-      <h2 className="text-center text-xl font-semibold">2024년 9월</h2>
+      <h2 className="text-center text-xl font-semibold text-zinc-400">&lt; 2024년 9월 &gt;</h2> 
 
         {/* 주차별 획득한 펫 카드들 */}
-        {petRecords.map((pet, index) => (
-          <PetRecord
-            key={index}
-            week={pet.week}
-            petName={pet.petName}
-            wins={pet.wins}
-            losses={pet.losses}
-            petImageUrl={pet.petImageUrl}
-            onClick={() => handleCardClick(pet)}
-          />
-        ))}
+          {petRecords.map((pet, index) => (
+            <PetRecord
+              key={index}
+              week={pet.week}
+              petName={pet.petName}
+              wins={pet.wins}
+              losses={pet.losses}
+              petImageUrl={pet.petImageUrl}
+              onClick={() => handleCardClick(pet)}  // 클릭시 모달 오픈
+              // 화면 크기에 따른 카드의 너비와 높이 조정
+              style={{
+                width: `${80 * dvw}px`,  // 카드 너비를 뷰포트 너비의 30%로 설정
+                height: `${20 * dvh}px`,  // 카드 높이를 뷰포트 높이의 20%로 설정
+              }}
+            />
+          ))}
       {/* 펫 상세 정보 모달 */}
       <PetDetailModal
         isOpen={isModalOpen}
@@ -66,7 +75,7 @@ const RecordSection: React.FC = () => {
         petIndex={selectedPet.petIndex}  // 예시로 No.1
         petImageUrl={selectedPet.petImageUrl}
         petTrait={selectedPet.petTrait}  // 예시로 food 특성
-      />
+    />
     </section>
   );
 };
