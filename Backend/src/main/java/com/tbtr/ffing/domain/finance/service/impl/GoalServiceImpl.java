d0eb91be230c495c97cd36962cde42c10be14112
김해엽 <rjssod2@naver.com>
2024-10-02 16:49:16 +0900

    feat: 목표 설정 페이지에 필요한 api 구현 - 목표 자산액의 상,하한선과 추천 금액을 제공. - 목표 소비액 설정에 필요한 현재 총 자산, 남은 개월 수, 고정 수입 제공

--- Backend/src/main/java/com/tbtr/ffing/domain/finance/service/impl/GoalServiceImpl.java
+++ Backend/src/main/java/com/tbtr/ffing/domain/finance/service/impl/GoalServiceImpl.java
@@ -1,11 +1,15 @@
 package com.tbtr.ffing.domain.finance.service.impl;
 
 import com.tbtr.ffing.domain.finance.dto.request.goal.GoalReq;
+import com.tbtr.ffing.domain.finance.dto.response.goal.GoalDetailRes;
 import com.tbtr.ffing.domain.finance.dto.response.goal.GoalRes;
 import com.tbtr.ffing.domain.finance.dto.response.goal.SpendingRes;
 import com.tbtr.ffing.domain.finance.entity.Goal;
 import com.tbtr.ffing.domain.finance.repository.GoalRepository;
 import com.tbtr.ffing.domain.finance.service.GoalService;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDate;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,35 +23,65 @@ public class GoalServiceImpl implements GoalService {
     @Override
     @Transactional
     public GoalRes setGoal(GoalReq goalReq) {
-        // 1. 목표 자산 유효성 체크
-        // 목표 자산 > 고정 수입 * 남은 개월 수 -> 불가능
-
-        // 2. 목표 달성액 저장
-        Goal goal = GoalReq.to(goalReq);
+        // 목표 자산, 소비액 저장
+        Goal goal = GoalReq.goalTo(goalReq);
+        Goal spending = GoalReq.spendingTo(goalReq);
         goalRepository.save(goal);
+        goalRepository.save(spending);
 
-        String goalBalance = goal.getBalance().toString();
-        // 목표 소비액 파트 추가 해야함. (목표 자산 - 현재 총 자산) / 남은 개월 수
-        SpendingRes spending = SpendingRes.builder()
-                                          .leftMonths(12 - goal.getCreatedAt().getMonthValue() + 1)
-                                          .spendingBalance("100").build();
+        SpendingRes spendingRes = SpendingRes.builder()
+                                             .leftMonths(12 - goal.getCreatedAt().getMonthValue() + 1)
+                                             .spendingBalance(goal.getBalance().toString()).build();
+
         return GoalRes.builder()
-                      .goalBalance(goalBalance)
-                      .spending(spending).build();
+                      .goalBalance(goal.getBalance().toString())
+                      .spending(spendingRes).build();
     }
 
     @Override
     @Transactional
     public SpendingRes setSpending(GoalReq goalReq) {
-        // 1. 목표 소비액 유효성 체크
-        // 목표 소비액 < (목표 자산 - 현재 총 자산) / 남은 개월 수 -> 불가능
-
-        // 2. 목표 소비액 저장
-        Goal goal = GoalReq.to(goalReq);
+        // 목표 소비액 저장
+        Goal goal = GoalReq.spendingTo(goalReq);
         goalRepository.save(goal);
 
         return SpendingRes.builder()
                           .leftMonths(12 - goal.getCreatedAt().getMonthValue() + 1)
-                          .spendingBalance(goalReq.getBalance()).build();
+                          .spendingBalance(goal.getBalance().toString()).build();
     }
+
+    @Override
+    @Transactional
+    public GoalDetailRes getGoal(Long userId) {
+        // 현재 총 자산 : asset 테이블에서 가져오기
+        BigDecimal totalAsset = new BigDecimal(10000000);
+
+        // 고정 수입 : 현재 기준 한 달 전 account_transaction 에서 transaction_type_name = 입금(고정) 에서 가져오기
+        BigDecimal fixedIncome = new BigDecimal(4000000);
+
+        // 남은 개월 수
+        int leftMonths = 12 - LocalDate.now().getMonthValue() + 1;
+
+        // 최대 목표 자산
+        BigDecimal totalAmount = totalAsset.add(fixedIncome.multiply(new BigDecimal(leftMonths)));
+
+        // 상한선
+        String upperLimitBalance = totalAmount.toString();
+
+        // 하한선
+        String lowerLimitBalance = totalAsset.toString();
+
+        // 추천 금액 : 수입의 50%
+        String recommendedGoalBalance =
+                totalAsset.add(fixedIncome.multiply(new BigDecimal(leftMonths)).multiply(new BigDecimal(
+                        "10")).divide(new BigDecimal("20"), 0, RoundingMode.CEILING)).toString();
+        return GoalDetailRes.builder()
+                            .totalAsset(totalAsset.toString())
+                            .leftMonths(leftMonths)
+                            .fixedIncome(fixedIncome.toString())
+                            .recommendedGoalBalance(recommendedGoalBalance)
+                            .lowerLimitBalance(lowerLimitBalance)
+                            .upperLimitBalance(upperLimitBalance).build();
+    }
+
 }
