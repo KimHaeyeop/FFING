2c2f2f6f268132480af874875d802a8e422e4245
김해엽 <rjssod2@naver.com>
2024-10-02 11:07:18 +0900

    feat: 목표 자산, 소비액 로직 일부 구현 - 목표 자산, 소비액 유효성 체크 파트 추가 필요 - 고정 수입액 필드 추가 필요

---
+++ Backend/src/main/java/com/tbtr/ffing/domain/finance/controller/GoalController.java
@@ -0,0 +1,56 @@
+package com.tbtr.ffing.domain.finance.controller;
+
+
+import com.tbtr.ffing.domain.finance.dto.request.goal.GoalReq;
+import com.tbtr.ffing.domain.finance.dto.response.goal.GoalRes;
+import com.tbtr.ffing.domain.finance.dto.response.goal.SpendingRes;
+import com.tbtr.ffing.domain.finance.service.GoalService;
+import com.tbtr.ffing.global.common.dto.Response;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/goal")
+@RequiredArgsConstructor
+public class GoalController {
+
+    private final GoalService goalService;
+
+    @GetMapping("/test")
+    public ResponseEntity<?> test() {
+        return ResponseEntity.ok("goal test");
+    }
+
+    /**
+     * 목표 달성액 설정
+     */
+    @PostMapping("/set-goal")
+    public ResponseEntity<?> setGoal(@RequestBody GoalReq goalReq) {
+        GoalRes goalRes = goalService.setGoal(goalReq);
+
+        Response<Object> response = Response.builder()
+                .code(200L)
+                .message("목표 달성액이 설정되었습니다.")
+                .result(goalRes).build();
+        return ResponseEntity.ok(response);
+    }
+
+    /**
+     * 목표 소비액 설정
+     */
+    @PostMapping("/set-spending")
+    public ResponseEntity<?> setSpending(@RequestBody GoalReq goalReq) {
+        SpendingRes spendingRes = goalService.setSpending(goalReq);
+
+        Response<Object> response = Response.builder()
+                                            .code(200L)
+                                            .message("목표 소비액이 설정되었습니다.")
+                                            .result(spendingRes).build();
+        return ResponseEntity.ok(response);
+    }
+}
---
+++ Backend/src/main/java/com/tbtr/ffing/domain/finance/repository/GoalRepository.java
@@ -0,0 +1,7 @@
+package com.tbtr.ffing.domain.finance.repository;
+
+import com.tbtr.ffing.domain.finance.entity.Goal;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GoalRepository extends JpaRepository<Goal, Long> {
+}
