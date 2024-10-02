a5bd47f2103fa6d42f9769aa032c1d2ab6fe20fd
탁인혁 <dlsgur1343@gmail.com>
2024-10-02 16:12:59 +0900

    feat: 펫 관련 Entity 세팅

--- Backend/src/main/java/com/tbtr/ffing/domain/game/entity/PetType.java
+++ Backend/src/main/java/com/tbtr/ffing/domain/game/entity/PetType.java
@@ -3,25 +3,28 @@ package com.tbtr.ffing.domain.game.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.time.LocalDate;
+
 @Getter
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder
 @Entity
-@Table(name = "pet_list")
-public class PetList {
+@Table(name = "pet_type")
+public class PetType {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long petId;
+    private Long petTypeId;
 
-    @Column(nullable = false, length = 3)
-    private String petCode;
+    @Column(nullable = false, length = 2)
+    private String typeCode;
 
     @Column(nullable = false, length = 20)
-    private String petName;
+    private String typeName;
 
     @Column(nullable = false, length = 255)
     private String imageUrl;
+
 }
