a507be8ccdf5c1a1121fbc4b87bbbbebd03f6d43
김해엽 <rjssod2@naver.com>
2024-10-06 19:04:34 +0900

    refactor : 코드 정리

--- Backend/src/main/java/com/tbtr/ffing/global/auth/util/JWTUtil.java
+++ Backend/src/main/java/com/tbtr/ffing/global/auth/util/JWTUtil.java
@@ -1,8 +1,6 @@
 package com.tbtr.ffing.global.auth.util;
 
 import com.tbtr.ffing.domain.user.dto.CustomUserDetails;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import java.nio.charset.StandardCharsets;
 import java.util.Date;
@@ -25,44 +23,14 @@ public class JWTUtil {
                 Jwts.SIG.HS256.key().build().getAlgorithm());
     }
 
-    public String getUserId(String token) {
-        try {
-            return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload()
-                       .get("userId", String.class);
-        } catch (ExpiredJwtException e) {
-            // 만료된 토큰에서도 userId를 추출할 수 있도록 예외 처리
-            return e.getClaims().get("userId", String.class);
-        } catch (JwtException e) {
-            // 다른 JWT 관련 예외 발생 시 처리
-            System.out.println("JWT parsing error: " + e.getMessage());
-            return null;
-        }
+    public Long getUserId(String token) {
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload()
+                   .get("userId", Long.class);
     }
 
     public Boolean isExpired(String token) {
-        System.out.println("JWTUtil: Checking if the token is expired..."); // 로그 추가
-        System.out.println("현재시간: " + new Date(System.currentTimeMillis()));
-
-        Date expirationDate = null;
-        try {
-            expirationDate = Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload()
-                                 .getExpiration();
-        } catch (ExpiredJwtException e) {
-            System.out.println("Token is expired: " + e.getMessage());
-            return true; // 토큰이 만료된 경우 true 반환
-        } catch (JwtException e) {
-            System.out.println("JWT parsing error: " + e.getMessage());
-            return true; // 파싱 에러가 발생한 경우도 true 반환
-        }
-
-        System.out.println("expirationDate: " + expirationDate);
-        Date currentDate = new Date();
-
-        // 토큰의 만료 날짜와 현재 날짜를 출력
-        System.out.println("Token Expiration Date: " + expirationDate);
-        System.out.println("Current Date: " + currentDate);
-
-        return expirationDate.before(currentDate);
+        return Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token).getPayload().getExpiration()
+                   .before(new Date());
     }
 
     public String createJwt(String category, CustomUserDetails userDetails) {
@@ -73,7 +41,7 @@ public class JWTUtil {
         return Jwts.builder()
                    .claims(userDetails.getClaims())
                    .issuedAt(new Date(System.currentTimeMillis()))
-                   .expiration(new Date(System.currentTimeMillis() + expirationPeriod * 500L)) // 30초
+                   .expiration(new Date(System.currentTimeMillis() + expirationPeriod * 1000L))
                    .signWith(secretKey)
                    .compact();
     }
--- Backend/src/main/java/com/tbtr/ffing/global/redis/repository/RedisRefreshTokenRepository.java
+++ Backend/src/main/java/com/tbtr/ffing/global/redis/repository/RedisRefreshTokenRepository.java
@@ -1,9 +1,7 @@
 package com.tbtr.ffing.global.redis.repository;
 
 import com.tbtr.ffing.global.redis.component.RedisRefreshToken;
-import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 
 public interface RedisRefreshTokenRepository extends CrudRepository<RedisRefreshToken, String> {
-    Optional<RedisRefreshToken> findByUserId(String userId);
 }
