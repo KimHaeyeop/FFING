7c63d6ab299e168c8d4709f6f1534907e50f7e82
김해엽 <rjssod2@naver.com>
2024-10-06 19:03:02 +0900

    feat : reissue api 추가 - access token 만료시 refresh token 기반 token 재발급 api - RTR 기법 활용 : access token 재발급시 refresh token 도 함께 재발급

--- Backend/src/main/java/com/tbtr/ffing/domain/user/service/AuthService.java
+++ Backend/src/main/java/com/tbtr/ffing/domain/user/service/AuthService.java
@@ -4,6 +4,8 @@ import com.tbtr.ffing.domain.user.dto.request.UserInfoReq;
 import com.tbtr.ffing.domain.user.dto.request.UserSigninReq;
 import com.tbtr.ffing.domain.user.dto.response.SigninRes;
 import com.tbtr.ffing.domain.user.dto.response.UserInfoRes;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public interface AuthService {
 
@@ -14,4 +16,6 @@ public interface AuthService {
     Boolean isEmialDuplication(String email);
 
     Boolean isNicknameDuplication(String nickname);
+
+    void reissue(HttpServletRequest request, HttpServletResponse response);
 }
