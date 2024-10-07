ebce1a06eacabca0eb8f42cfc68e29f8e057922d
Myeonghwa Kim <mhwazz@gmail.com>
2024-10-07 20:28:15 +0900

    feat: 카드 지출발생 시 알림 전송
    - 카드지출 발생 시 알림 전송

--- Frontend/FFING/dev-dist/sw.js
+++ Frontend/FFING/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.thi2s1tc5l"
+    "revision": "0.v1u765jl85g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
