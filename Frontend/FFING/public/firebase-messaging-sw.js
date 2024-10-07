bc1c52a8af50dd5a39f0c2b639d66ab5b81f112b
변재호 <bjho606@naver.com>
2024-10-07 19:19:25 +0900

    chore: fcm 설정 변경

--- Frontend/FFING/public/firebase-messaging-sw.js
+++ Frontend/FFING/public/firebase-messaging-sw.js
@@ -1,7 +1,5 @@
-importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
-importScripts(
-  "https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js"
-);
+importScripts("https://www.gstatic.com/firebasejs/9.5.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.5.0/firebase-messaging-compat.js");
 
 // Firebase 구성 객체를 사용해 Firebase를 초기화합니다.
 const firebaseConfig = {
@@ -19,56 +17,12 @@ firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
-  console.log("Received background message ", payload);
-  const { title, body } = payload.notification;
-  const options = {
-    body: body,
-    icon: "./logo.png",
-    badge: "./logo.png",
+  console.log("[firebase-messaging-sw.js] Received background message ", payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: "/firebase-logo.png", // 원하는 아이콘으로 변경
   };
 
-  self.registration.showNotification(title, options);
-});
-
-self.addEventListener("install", function (e) {
-  console.log("fcm sw install..");
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", function (e) {
-  console.log("fcm sw activate..");
-});
-
-self.addEventListener("notificationclick", (event) => {
-  const urlToOpen = new URL(`http://localhost:5173`);
-
-  event.waitUntil(
-    clients
-      .matchAll({
-        type: "window",
-        includeUncontrolled: true,
-      })
-      .then((windowClients) => {
-        let foundWindowClient = null;
-
-        for (let i = 0; i < windowClients.length; i++) {
-          const client = windowClients[i];
-
-          if (client.url.includes("localhost:5173") && "focus" in client) {
-            foundWindowClient = client;
-            break;
-          }
-        }
-
-        if (foundWindowClient) {
-          return foundWindowClient.focus().then((focusedClient) => {
-            if ("navigate" in focusedClient) {
-              focusedClient.postMessage(urlToOpen.href);
-            }
-          });
-        } else if (clients.openWindow) {
-          return clients.openWindow(urlToOpen.href);
-        }
-      })
-  );
+  self.registration.showNotification(notificationTitle, notificationOptions);
 });
