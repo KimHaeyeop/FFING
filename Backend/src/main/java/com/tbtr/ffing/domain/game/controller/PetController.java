6d6497fce01a210a8b362e2517fff84dd26d9aa2
탁인혁 <dlsgur1343@naver.com>
2024-10-08 14:50:26 +0900

    feat: 펫 생성 관련 API 개발

--- Backend/src/main/java/com/tbtr/ffing/domain/game/controller/PetController.java
+++ Backend/src/main/java/com/tbtr/ffing/domain/game/controller/PetController.java
@@ -1,5 +1,8 @@
 package com.tbtr.ffing.domain.game.controller;
 
+import com.tbtr.ffing.domain.finance.dto.response.expense.WeeklyCategoryExpenseRes;
+import com.tbtr.ffing.domain.finance.service.ExpenseService;
+import com.tbtr.ffing.domain.finance.service.GoalService;
 import com.tbtr.ffing.domain.game.dto.response.PetCollectionRes;
 import com.tbtr.ffing.domain.game.dto.response.PetHistoryRes;
 import com.tbtr.ffing.domain.game.dto.response.PetInfoRes;
@@ -59,4 +62,19 @@ public class PetController {
 
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
+
+    @PostMapping("")
+    public ResponseEntity<Object> insertPetInfo(@RequestParam long userId) {
+        Response<Object> response = null;
+
+        if (petService.createPetInfo(userId)) {
+            response = Response.builder()
+                    .code(200L)
+                    .message("성공")
+                    .build();
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
 }
