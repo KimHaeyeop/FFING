a82951b60fbb53a89623f3d9655a6704504c6814
이규석 <didie8520@gmail.com>
2024-10-08 18:15:44 +0900

    refactor: 범례를 클릭했을 때, 필터링 수행

--- Frontend/FFING/src/components/Spending/CategoryChart.tsx
+++ Frontend/FFING/src/components/Spending/CategoryChart.tsx
@@ -13,7 +13,6 @@ interface MonthlyCategorySpending {
   target: number;
 }
 
-
 interface SpendingCategoryChartProps {
   onClick: (category: string) => void; // 카테고리를 클릭했을 때 호출하는 함수
 }
@@ -38,7 +37,7 @@ const SpendingCategoryChart: React.FC<SpendingCategoryChartProps> = ({ onClick }
   }, []);
 
   // API 연동 필요
-  const targetSpending = 100000
+  const targetSpending = 100000;
 
   // FINANCE, FOOD_BAKERY, LIFE_CULTURE, SHOPPING, TRANSPORTATION, OVERSEAS
   // key를 문자열로 사용 가능하게 변경
@@ -48,27 +47,20 @@ const SpendingCategoryChart: React.FC<SpendingCategoryChartProps> = ({ onClick }
     'LIFE_CULTURE': '생활/문화',
     'SHOPPING': '쇼핑',
     'TRANSPORTATION': '교통',
-  }
+  };
 
   // 해외 카테고리 삭제 전까지 이 걸로 진행해야 함
   const filteredData = spendingData
-  .filter(item => item.category !== 'OVERSEAS')
-  .sort((a, b) => b.totalAmount - a.totalAmount); // 차트 정렬하기
+    .filter(item => item.category !== 'OVERSEAS')
+    .sort((a, b) => b.totalAmount - a.totalAmount); // 차트 정렬하기
 
-  // 클릭 시 카테고리 필터링 및 전달하는 함수
-  const handleClick = (event: React.MouseEvent<HTMLCanvasElement>, elements: ActiveElement[]) => {
-    // 다른 곳을 클릭하면
-    if (elements.length > 0) {
-      const chart = elements[0].element.$context.chart;
-      const index = elements[0].index;
-      const categoryLabel = chart.data.labels[index];
-      const category = Object.keys(mapKrUs).find(key => mapKrUs[key] === categoryLabel);  // 카테고리 매핑
-      setHighlightedCategory(category)  // 클릭된 카테고리 강조
-      onClick(category); // 카테고리 전달
-    } else {
-      onClick('') // 클릭 안 된 경우 빈 값 전달
-    }
-  };  
+  // 범례 클릭 시 카테고리 필터링 및 전달하는 함수
+  const handleLegendClick = (event: React.MouseEvent<HTMLLIElement>, legendItem: any) => {
+    const categoryLabel = legendItem.text.split(' ')[0];
+    const category = Object.keys(mapKrUs).find(key => mapKrUs[key] === categoryLabel);  // 카테고리 매핑
+    setHighlightedCategory(category!);  // 클릭된 카테고리 강조
+    onClick(category!); // 카테고리 전달
+  };
 
   const config = {
     data: {
@@ -87,39 +79,43 @@ const SpendingCategoryChart: React.FC<SpendingCategoryChartProps> = ({ onClick }
         },
       ],
     },
-    // 차트 중앙에 자금 초과 여부 렌더링 필요
     // 범례 label과 value 사이의 거리 이격 필요
     options: {
       responsive: false,
       plugins: {
         legend: {
+          onClick: handleLegendClick, // 범례 클릭 시 실행되는 함수
           position: 'right' as const, // 차트 오른편에 범례
           labels: {
             boxWidth: 15, // 범례 색상 공간의 너비 수정
             // 범례 색상의 테두리 삭제 필요
             generateLabels: (chart: Chart) => {
-              const data = chart.data
+              const data = chart.data;
               return data.labels.map((label, i) => {
-                const value = data.datasets[0].data[i]
-                const formattedValue  = value.toLocaleString(undefined, {maximumFractionDigits: 0})
+                const value = data.datasets[0].data[i];
+                const formattedValue = value.toLocaleString(undefined, { maximumFractionDigits: 0 });
                 return {
                   text: `${label} ${formattedValue}원`,
                   fillStyle: data.datasets[0].backgroundColor[i],
                   hidden: false,
-                  class: label === highlightedCategory ? 'font-galmuri-11-bold' : '', // 이거 아직 안 되서 수정해야 함
-                }
-              })
-            }
+                  font: {
+                    weight: label === mapKrUs.highlightedCategory ? 'bold' : 'normal', // 클릭된 범례를 bold체로 변경
+                  },
+                  className: label === mapKrUs.highlightedCategory ? 'font-galmuri-11-bold' : '', // 이거 아직 안 되서 수정해야 함
+                  index: i,
+                };
+              });
+            },
+            
           },
         },
         tooltip: {
           enabled: false,
         },
       },
-      // 클릭하면 해당 부분이 확대되고 해당 항목의 거래 내역만 렌더링
-      onClick: handleClick,
-    }
-  }
+    },
+  };
+
   return (
     <div className="h-full w-full flex justify-center items-center">
       <Doughnut {...config} />
