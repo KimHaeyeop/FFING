6c0a2493f2c14e692320b2079a026b18c7c0e55a
이규석 <didie8520@gmail.com>
2024-10-08 16:18:35 +0900

    feat: 게임 메인 페이지에서 현재 펫과 승리 렌더링하기

--- Frontend/FFING/src/hook/usePetStats.ts
+++ Frontend/FFING/src/hook/usePetStats.ts
@@ -13,6 +13,8 @@ export const usePetStats = (userId: string) => {
           '교통': responsePetStats.data.result.currentPetInfo.transportationStat,
           '생활/문화': responsePetStats.data.result.currentPetInfo.lifeCultureStat,
           '금융': responsePetStats.data.result.currentPetInfo.financeStat,
+          'petCode': responsePetStats.data.result.currentPetInfo.petCode,
+          'winCount': responsePetStats.data.result.currentPetInfo.winCount,
         },
         previousWeek: {
           '식비': responsePetStats.data.result.beforePetInfo.foodBakeryStat,
@@ -24,8 +26,8 @@ export const usePetStats = (userId: string) => {
       }
     },
     initialData: {
-      currentWeek: { '식비': 0, '쇼핑': 0, '교통': 0, '생활/문화': 0, '금융': 0 },
-      previousWeek: { '식비': 0, '쇼핑': 0, '교통': 0, '생활/문화': 0, '금융': 0 },
+      currentWeek: { '식비': 0, '쇼핑': 0, '교통': 0, '생활/문화': 0, '금융': 0, 'petCode': '000', 'winCount': 0},
+      previousWeek: { '식비': 0, '쇼핑': 0, '교통': 0, '생활/문화': 0, '금융': 0, }, 
     }
   });
 };
