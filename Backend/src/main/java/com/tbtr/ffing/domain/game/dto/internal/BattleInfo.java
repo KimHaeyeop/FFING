365bad399653a9e7015c3829b48d6d565afba215
원민혁 <wmy4534@gmail.com>
2024-10-09 18:13:42 +0900

    hotfix: java 날짜 직렬화/역직렬화 설정

--- Backend/src/main/java/com/tbtr/ffing/domain/game/dto/internal/BattleInfo.java
+++ Backend/src/main/java/com/tbtr/ffing/domain/game/dto/internal/BattleInfo.java
@@ -1,5 +1,9 @@
 package com.tbtr.ffing.domain.game.dto.internal;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import com.tbtr.ffing.domain.game.entity.PetInfo;
 import lombok.*;
 
@@ -17,6 +21,8 @@ public class BattleInfo {
     private BattlePetInfo battlePet1;
     private BattlePetInfo battlePet2;
 
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime createdAt;
 
     public static BattleInfo from (String matchId, BattlePetInfo petInfo1, BattlePetInfo petInfo2, LocalDateTime createdAt) {
