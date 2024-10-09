be7e5305c144ec9e89936cc7974fa9a6b7f95847
Myeonghwa Kim <mhwazz@gmail.com>
2024-10-09 22:04:10 +0900

    refactor: 메인 UI 개선
    - 메인 UI 개선

--- Frontend/FFING/src/components/Asset/HorizontalBarChart.tsx
+++ Frontend/FFING/src/components/Asset/HorizontalBarChart.tsx
@@ -1,27 +1,30 @@
-import React, {useEffect} from 'react';
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, LinearScale } from 'chart.js';
-import { getTotalAsset } from '../../api/AssetApi';
-import { getTargetPropertySpending } from '../../api/goalApi';
+import React, { useEffect } from "react";
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  LinearScale,
+} from "chart.js";
+import { getTotalAsset } from "../../api/AssetApi";
+import { getTargetPropertySpending } from "../../api/goalApi";
 
 ChartJS.register(Title, Tooltip, Legend, BarElement, LinearScale);
 
-
 const HorizontalBarChart: React.FC = () => {
-  // API 연동 필요
-  const [property, setProperty] = React.useState(0); // 자산
-  const [target, setTarget] = React.useState(0);  // 목표액
-  const progressPercentage = (property / target) * 100; // 비율
+  const [property, setProperty] = React.useState(0);
+  const [target, setTarget] = React.useState(0);
+  const progressPercentage = (property / target) * 100;
 
-  // 이번 달 지출액을 가져오는 함수
   const fetchData = async () => {
     try {
-      // 적절한 유저 이름 설정 필요
-      const response = await getTotalAsset('1');
-      const responseGoal = await getTargetPropertySpending('1')
+      const response = await getTotalAsset("1");
+      const responseGoal = await getTargetPropertySpending("1");
       setProperty(response.data.result.currentAsset.totalAsset);
       setTarget(responseGoal.data.result.goalBalance);
     } catch (error) {
-      console.error('Error fetching certain spending data:', error);
+      console.error("Error fetching certain spending data:", error);
     }
   };
 
@@ -32,15 +35,21 @@ const HorizontalBarChart: React.FC = () => {
   return (
     <div className="h-full w-full flex flex-col justify-center items-center">
       {/* 전체 목표 바 */}
-      <div className="w-4/5 h-8 rounded-e-2xl my-2" style={{ backgroundColor: '#828181' }}>
+      <div className="w-4/5 h-8 bg-[#828181] rounded-2xl overflow-hidden">
         {/* 달성한 자산 바 */}
         <div
-          className="h-full transition-width duration-300 ease-in-out rounded-e-2xl"
-          style={{ width: `${progressPercentage}%`, backgroundColor: '#FFD55E' }}
+          className="h-full transition-all duration-300 ease-in-out"
+          style={{
+            width: `${progressPercentage}%`,
+            backgroundColor: "#FFD55E",
+          }}
         />
       </div>
+
       {/* 목표 달성까지 남은 금액 표시 */}
-      <p className="mb-2">{(target - property).toLocaleString()}원 남았습니다...</p>
+      <p className="mt-3">
+        {(target - property).toLocaleString()}원 남았습니다...
+      </p>
     </div>
   );
 };
