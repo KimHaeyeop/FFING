7172c779e75b8c45a0bf91fadb9ea1c5b41cd3c4
Myeonghwa Kim <mhwazz@gmail.com>
2024-10-09 15:00:11 +0900

    refactor: 지출 캘린더 UI 수정
    - 지출 캘린더 UI 수정

--- Frontend/FFING/dev-dist/sw.js
+++ Frontend/FFING/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.hujch7h12hg"
+    "revision": "0.7et3f33ve0o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
