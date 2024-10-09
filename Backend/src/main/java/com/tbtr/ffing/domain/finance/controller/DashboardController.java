e03d5d8cd45fccf5070a5cef0a9942c9b11533f1
김해엽 <rjssod2@naver.com>
2024-10-10 02:49:01 +0900

    refactor : dashboard auth 적용

--- Backend/src/main/java/com/tbtr/ffing/domain/finance/controller/DashboardController.java
+++ Backend/src/main/java/com/tbtr/ffing/domain/finance/controller/DashboardController.java
@@ -2,12 +2,13 @@ package com.tbtr.ffing.domain.finance.controller;
 
 import com.tbtr.ffing.domain.finance.dto.response.dashboard.MainDashboardRes;
 import com.tbtr.ffing.domain.finance.service.DashboardService;
+import com.tbtr.ffing.domain.user.dto.CustomUserDetails;
 import com.tbtr.ffing.global.common.dto.Response;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,20 +21,11 @@ public class DashboardController {
      * 홈 화면
      */
     @GetMapping("/main")
-    public ResponseEntity<?> mainPage(@RequestParam("userId") Long userId) {
-        MainDashboardRes response = dashboardService.mainPage(userId);
+    public ResponseEntity<?> mainPage(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        MainDashboardRes response = dashboardService.mainPage(userDetails.getUserId());
         return ResponseEntity.ok(Response.builder()
                 .code(200L)
                 .message("성공")
                 .result(response).build());
     }
 }
-/**
- * 유저 이름
- * 목표 달성
- * 목표 달성까지 00원 남았습니다.
- * 목표 자산액, 총 자산액
- * 현재 펫 -> 코드
- * 이번달 총 소비
- * 카테고리 별 소비
- */
