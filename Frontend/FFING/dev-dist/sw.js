3e03cb74469da331ce8668d23fdf890acbd82a70
원민혁 <wmy4534@gmail.com>
2024-10-10 02:38:23 +0900

    fix: conflict

--- Frontend/FFING/dev-dist/sw.js
+++ Frontend/FFING/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.4lfi8u529p8"
+    "revision": "0.jj38bgdd558"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
