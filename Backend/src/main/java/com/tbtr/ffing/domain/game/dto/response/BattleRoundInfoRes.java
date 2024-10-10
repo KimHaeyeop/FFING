26a53ae3778f678e47379ff604aa5ec726a89082
변재호 <bjho606@naver.com>
2024-10-10 14:50:46 +0900

    refactor: 배틀 턴 정보 변경
    - matchId, 끝났는지, 각 펫 정보
    	- 펫 정보 : 펫 id, 먼저 때렸는지, 공격 번호, 때린 데미지, 때린
    	  데미지 유효타 정보, 공격 받은 이후 본인 데미지, 총 스탯,
    	  카테고리별 스탯

--- Backend/src/main/java/com/tbtr/ffing/domain/game/dto/response/BattleRoundInfoRes.java
+++ Backend/src/main/java/com/tbtr/ffing/domain/game/dto/response/BattleRoundInfoRes.java
@@ -1,6 +1,7 @@
 package com.tbtr.ffing.domain.game.dto.response;
 
 import com.tbtr.ffing.domain.game.dto.internal.BattlePetInfo;
+import com.tbtr.ffing.domain.game.dto.internal.BattleRoundPetInfo;
 import lombok.*;
 
 @Getter
@@ -12,11 +13,8 @@ import lombok.*;
 public class BattleRoundInfoRes {
     private String matchId;
 
-    private Long firstToMovePetId;      // 먼저 공격하는 사람
-    private int pet1AttackNum;
-    private String pet1DamageStatus;    // 유효타 여부 (Good, Normal, Bad)
-    private int pet2AttackNum;
-    private String pet2DamageStatus;
-    private BattlePetInfo pet1Info;     // 모든 공격처리를 마친 후의 펫 상태
-    private BattlePetInfo pet2Info;     // 모든 공격처리를 마친 후의 펫 상태
+    private boolean isFinished;
+    private BattleRoundPetInfo pet1Info;
+    private BattleRoundPetInfo pet2Info;
+
 }
